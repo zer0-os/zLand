af05376a4d8618e6f79a63b56977809805a561d2
durienb <damien@wilderworld.com>
2024-09-21 00:00:46 -0600

    freeze

--- ignition/modules/BondingToken.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const BondingTokenModule = buildModule("BondingTokenModule", (m) => {
-  const bondingToken = m.contract("BondingTokenLinear");
-
-  return { bondingToken };
-});
-
-export default {BondingTokenModule};
--- ignition/modules/Refinery.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const RefineryModule = buildModule("RefineryModule", (m) => {
-  const refinery = m.contract("Refinery");
-
-  return { refinery };
-});
-
-export default {RefineryModule};
